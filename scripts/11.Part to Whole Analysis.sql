321db5ef462729bf2febc45118d9e3dfe6c2b053
shaliniselvam24 <shaliniselvamit@gmail.com>
2025-12-26 20:24:48 +0530

    Update 11.Part to Whole Analysis.sql

--- scripts/11.Part to Whole Analysis.sql
+++ scripts/11.Part to Whole Analysis.sql
@@ -1,1 +1,31 @@
+/*
+===============================================================================
+Part-to-Whole Analysis
+===============================================================================
+Purpose:
+    - To compare performance or metrics across dimensions or time periods.
+    - To evaluate differences between categories.
+    - Useful for A/B testing or regional comparisons.
 
+SQL Functions Used:
+    - SUM(), AVG(): Aggregates values for comparison.
+    - Window Functions: SUM() OVER() for total calculations.
+===============================================================================
+*/
+
+--categories contribute the most to overall sales?
+
+with category_sales as (select
+p.category,
+sum(f.sales_amount) as total_sales
+from gold.fact_sales f
+left join gold.dim_products p
+on f.product_key = p.product_key
+group by p.category)
+
+select category,
+total_sales,
+sum(total_sales) over() overall_sales,
+concat(round((cast(total_sales as float)/ sum(total_sales) over()) *100,2),'%') as percentage_of_total
+from category_sales
+order by total_sales desc 
