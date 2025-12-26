3df1c3a497efa6a0ed39270114fab2ac290616f9
shaliniselvam24 <shaliniselvamit@gmail.com>
2025-12-26 20:21:36 +0530

    Implement performance analysis SQL script
    Add SQL query for year-over-year and month-over-month performance analysis of products.

--- scripts/09.Performance Analysis.sql
+++ scripts/09.Performance Analysis.sql
@@ -1,1 +1,50 @@
+/*
+===============================================================================
+Performance Analysis (Year-over-Year, Month-over-Month)
+===============================================================================
+Purpose:
+    - To measure the performance of products, customers, or regions over time.
+    - For benchmarking and identifying high-performing entities.
+    - To track yearly trends and growth.
 
+SQL Functions Used:
+    - LAG(): Accesses data from previous rows.
+    - AVG() OVER(): Computes average values within partitions.
+    - CASE: Defines conditional logic for trend analysis.
+===============================================================================
+*/
+/*analyse yearly performance of products by comparing their
+sales to both avg sales per of prod and previous year's sales*/
+
+with yearly_product_sales as (
+select 
+year(f.order_date) as order_year,
+p.product_name,
+sum(f.sales_amount) as current_sales
+from
+gold.fact_sales f
+left join gold.dim_products p
+on f.product_key= p.product_key
+where order_date is not null
+group by year(f.order_date),p.product_name)
+
+select 
+order_year,
+product_name,
+current_sales,
+avg(current_sales) over(partition by product_name) as avg_of_sales
+, current_sales- avg(current_sales) over(partition by product_name) as diff_avg,
+case
+when current_sales- avg(current_sales) over(partition by product_name) >0 then 'above avg'
+when current_sales- avg(current_sales) over(partition by product_name) < 0 then 'below avg'
+else 'avg'
+end avg_change,
+---Year over Year(YOY) Analysis
+lag(current_sales) over(partition by product_name order by order_year) as py_sales,
+current_sales-lag(current_sales) over(partition by product_name order by order_year) as diff_py ,
+case when current_sales-lag(current_sales) over(partition by product_name order by order_year)>0 then 'Increase'
+when current_sales-lag(current_sales) over(partition by product_name order by order_year)<0 then 'Decrease'
+else 'none'
+end as avg_change
+from yearly_product_sales
+order by product_name,order_year
